d448de1c71d6bc99386fa5103be3c248627c2e28
SAKSHAM JOSHI <109752898+saksham-joshi@users.noreply.github.com>
2023-10-21 17:17:08 +0530

    Add files via upload

--- sron.cpp
+++ sron.cpp
@@ -2,48 +2,102 @@
 #include "headers/_char_.h"
 #include "headers/_convertor_.h"
 #include "headers/_exception_.h"
+#include "headers/_execution_engine_.h"
+#include "headers/_hashtable_.h"
 #include "headers/_input_.h"
 #include "headers/_math_.h"
 #include "headers/_string_.h"
+#include "headers/static_logs.h"
 #include "headers/_utility_.h"
 #include "headers/_vector_.h"
 
 #include <iostream>
 #include <fstream>
-#include<map>
-
-
-// function to validate variable names
+#include <map>
 
 int main(int argc, char **argv)
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(0);
     string str;
     try
     {
         ifstream code_file(argv[1]);
-        if(code_file.fail()){
-            throw FileNotFoundException;
+        if (code_file.fail())
+        {
+            DISPLAY_EXCEPTION("getting the file from the specified path.", 1);
         }
-        while(getline(code_file , str)){
+
+        while (getline(code_file, str))
+        {
+
             str = String().STRIP(str);
-            if(str.length() == 0){
+            if (str.length() == 0)
+            {
                 continue;
             }
-            string attribute = Utility().GET_ATTRIBUTE(str);
-            if(attribute == "Comment"){
+            else if(str == "{" || str == "}" ){
                 continue;
             }
 
+            String_and_Int obj = Executor::GET_ATTRIBUTE(str);
+            string attribute = obj.str;
+
+            if (Executor::CHECK_IF_ATTRIBUTE_IS_NUMBER(attribute))
+            {
+                continue;
+            }
+            else if(attribute == "args"){
+
+            }
+            else if (attribute == "comment")
+            {
+                continue;
+            }
+            else if (attribute == "condition")
+            {
+            }  
+            else if (attribute == "forloop")
+            {
+
+            }
+            else if(attribute == "name"){
+                
+            }
+            else if (attribute == "range")
+            {
+
+            }
+            else if (attribute == "return")
+            {
+
+            }
+            else if (attribute == "type")
+            {
+
+            }
+            else if (attribute == "variables")
+            {
+
+            }
+            else if (attribute == "whileloop")
+            {
+
+            }   
+            else
+            {
+                DISPLAY_EXCEPTION("mapping the attribute with the functionality.", 8);
+            }
+            Logs::INCREMENT_LINE_NUMBER() ;
         }
-        
-        
     }
-    catch (const char* excep )
+    catch (const char *excep)
     {
-        printf("%s",excep);
+        printf("%s", excep);
         exit(1);
+    }
+    catch (std::exception)
+    {
     }
     return 0;
 }
