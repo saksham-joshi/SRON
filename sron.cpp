7c22472fa8174f2fccff3cad6fc9fb17b4f0f442
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2024-11-29 23:53:14 +0530

    updated .cpp files

---
+++ sron.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2024 SAKSHAM JOSHI
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * You can freely redistribute it but cannot modify the source code without the permission from the author.
+ * @author- SAKSHAM JOSHI
+ * @Linkedin- @sakshamjoshi27
+ * @Twitter- @sakshamjoshi27
+ * @email- social.sakshamjoshi@gmail.com
+ */
+
+#ifndef SRON_H
+#define SRON_H
+
+#include "sron_interpreter/_execution_engine_.hpp"
+
+
+int main(int argc, char **argv)
+{
+    try
+    {
+
+        GarbageCollector::INITIALIZE();
+        
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(0);
+
+        std::cout<< std::boolalpha << std::fixed << std::setprecision(5);
+
+        // create a List to provide command line arguments to the user
+        ListPtr arglist = new List(0Ul);
+
+        for (int i = 1; i < argc; ++i)
+        {
+            arglist->PUSH_WITHOUT_COPY(new String(argv[i]));
+        }
+
+        AnyBind arglistBind(arglist, 0);
+
+        // starting the execution of the code
+        ExecutionEngine::MAIN(&arglistBind);
+
+        std::cout << "\n   _________________________________________\n   | Time taken by SRON : " << std::fixed << double(clock()) / double(CLOCKS_PER_SEC) << " seconds. |";
+       
+    }
+    catch(const NullPointerException &excep)
+    {
+        DISPLAY_EXCEPTION(excep.what(),NullAccessingException);
+        return 1;
+    }
+    catch (const ThrowException &excep)
+    {
+        DISPLAY_EXCEPTION(excep.message__.GET(), excep.excode__);
+        return 1;
+    }
+    catch (const std::bad_alloc &)
+    {
+        std::cerr << "\n+================================+\n"
+                       "|     System Out of Memory       |\n"
+                       "+================================+\n";
+
+                       return 1;
+    }
+    catch(const ExceptionThrowByUser& __excep)
+    {
+        __excep.DISPLAY_EXCEPTION();
+    }
+    catch (const std::exception &excep)
+    {
+        std::cerr << "\n | Error: " << excep.what() << '\n';
+
+        DISPLAY_EXCEPTION("compiling the source code. An Unexpected Error showed up. Mail the error to 'sron.language@gmail.com' along with source code and above mentioned error", NoException);
+        return 1;
+    }
+
+    #ifdef SRON_DEVELOPMENT_PHASE
+        // DESTROYING ALL GATHERED MEMORY .....
+        GarbageCollector::TOTAL_DESTRUCTION();
+        std::cout << "\n <|--- Total destruction done ---|> \n";
+    #endif
+
+    return 0;
+}
+
+#endif
---
+++ sronc.cpp
@@ -0,0 +1,61 @@
+#include "sron_compiler/_bytecode_generator_.hpp"
+
+int main(int argc, char **argv)
+{
+    try
+    {
+        if (Support::HANDLE_CMD_ARGUMENTS(argc, argv)) return 0;
+
+        Logs::SET_DIRECTORY_PATH();
+
+        Logs::SET_EXECUTABLE_PATH();
+
+        SemanticAnalyzer::LOAD_IN_BUILT_SET();
+
+        #ifndef SRON_DEVELOPMENT_PHASE
+            std::ios_base::sync_with_stdio(false);
+            std::cout.tie(NULL);
+        #endif
+
+        ByteCodeGenerator::GENERATE();
+
+        std::cout << "\n    =---> Time Taken : " << std::fixed << std::setprecision(5) << double(clock()) / double(CLOCKS_PER_SEC) << " seconds.\n";
+    }
+    catch (const ThrowException &excep)
+    {
+        unsigned long line_num = ULONG_MAX;
+
+        if(excep._file_index != ULONG_MAX)
+        {
+
+            ByteCodeGenerator::SHOW_CODE_GLIMPSE(excep._file_index);
+
+            line_num = ByteCodeGenerator::CALCULATE_LINE_NUMBER(excep._file_index);
+        }
+        
+        DISPLAY_ERROR(excep._message.GET(), excep._excode, line_num);
+    }
+    catch(const FileNameNotProvidedToCompiler &excep)
+    {
+        DISPLAY_ERROR(excep.what(), FileNameNotSpecifiedException);
+    }
+    catch(const SourceCodeFileNotFound &excep)
+    {
+        DISPLAY_ERROR(excep.what(), FileNotFoundException);
+    }
+    catch (const std::bad_alloc &excep)
+    {
+
+        std::cerr<<"\n+================================+\n"
+                     "|     System Out of Memory       |\n"
+                     "+================================+\n";
+    }
+    catch (const std::exception &excep)
+    {
+        std::cerr << "\n | Error: " << excep.what() << '\n';
+
+        DISPLAY_ERROR("compiling the source code. An Unexpected Error showed up. Mail the error on 'sron.language@gmail.com' with source code and above mentioned error", NoException);
+    }
+
+    return 0;
+}
