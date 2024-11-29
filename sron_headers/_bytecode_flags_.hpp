b469cc7eaaac7493c25c5dc606af249ee6657b54
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2024-11-29 23:54:38 +0530

    updated common headers

---
+++ sron_headers/_bytecode_flags_.hpp
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#ifndef _FLAG_H_
+#define _FLAG_H_
+
+typedef unsigned short flag_t;
+typedef long int byteindex_t;
+
+constexpr static const char* const __void_printable_string__ = "<#-Void-#>";
+
+
+#endif
