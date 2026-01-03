618bad215aec227a1d422339941cf7645a49494e
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2026-01-03 07:17:08 +0530

    Added: Execution Engine

---
+++ sron_interpreter/_execution_engine_.hpp
@@ -0,0 +1,18 @@
+// Closed-source file (_execution_engine_.hpp):
+/**
+ * SRON Programming Language - Proprietary Component
+ * 
+ * Copyright (c) 2025 Saksham Joshi. All Rights Reserved.
+ * 
+ * This file is part of SRON's proprietary ExecutionEngine and is not 
+ * licensed for public use, modification, or distribution. All rights reserved.
+ * 
+ * Unauthorized copying, reverse engineering, modification, or distribution 
+ * of this file, via any medium, is strictly prohibited.
+ * 
+ * For licensing inquiries, contact: sron.language@gmail.com
+ * 
+ * SPDX-License-Identifier: PROPRIETARY
+**/
+
+#include "_if_elif_else_.hpp"
