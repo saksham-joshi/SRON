ef2f3bd918591d0c7feaa2019e4a01621606a9b5
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2026-01-03 07:10:55 +0530

    Added: BytecodeWriter

---
+++ sron_compiler/headers/SemanticAnalyzer/BytecodeWriter/_bytecode_writer_.hpp
@@ -0,0 +1,18 @@
+// Closed-source file (_bytecode_writer_.hpp):
+/**
+ * SRON Programming Language - Proprietary Component
+ * 
+ * Copyright (c) 2025 Saksham Joshi. All Rights Reserved.
+ * 
+ * This file is part of SRON's proprietary BytecodeWriter and is not 
+ * licensed for public use, modification, or distribution. All rights reserved.
+ * 
+ * Unauthorized copying, reverse engineering, modification, or distribution 
+ * of this file, via any medium, is strictly prohibited.
+ * 
+ * For licensing inquiries, contact: sron.language@gmail.com
+ * 
+ * SPDX-License-Identifier: PROPRIETARY
+**/
+
+#include "_specific_writers_.hpp"
